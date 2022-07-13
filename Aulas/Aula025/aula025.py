a0fb5e2344bca67708fa21f173d082fa333d8c9a
Pedro Alves <pedroalves0409@gmail.com>
2022-07-13 11:50:01 -0300

    aula025

---
+++ Aulas/Aula025/aula025.py
@@ -0,0 +1,14 @@
+import os
+
+os.system('cls')
+
+# operadores de associação
+
+x = ['maçã', 'banana']
+
+# verifica se um determinado valor está em um objeto
+print('banana' in x)
+print('banana' not in x)
+
+x = [150, 348, 25]
+print(348 in x)
