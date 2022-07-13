7d1bf8eb37fbf126be99350437e32232829ea8e5
Pedro Alves <pedroalves0409@gmail.com>
2022-07-13 14:32:32 -0300

    novas aulas

--- Aulas/Aula033/aula033.py
+++ Aulas/Aula033/aula033.py
@@ -2,4 +2,29 @@ import os
 
 os.system('cls')
 
-nomes = ['Gabrie']
+# classificação de lista
+
+nomes = ['Gabriel', 'Danny', 'Arthur', 'João', 'Beatriz']
+
+numeros = [100, 50, 82, 23]
+
+# método sort
+
+nomes.sort()
+numeros.sort()
+
+print(nomes)
+print(numeros)
+
+nomes.sort(reverse = True) # ordem decrescente
+numeros.sort(reverse = True) # ordem decrescente
+
+print(nomes)
+print(numeros)
+
+def myfunction(n):
+    return abs(n - 50)
+
+numeros.sort(key = myfunction())
+
+print(numeros)
